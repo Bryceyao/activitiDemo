bc2ad619fd78ed676ebb5e090d84316d5517849b
Bryceyao <bryce.yao@wolaidai.com>
2017-06-08 09:23:09 +0800

    装好activiti-designer插件

---
+++ src/test/java/org/activiti/designer/test/ProcessTestHireProcessWithJpa.java
@@ -0,0 +1,36 @@
+package org.activiti.designer.test;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.FileInputStream;
+
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.test.ActivitiRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ProcessTestHireProcessWithJpa {
+
+	private String filename = "D:\\workSpace\\eclipseWorkFlow\\activitiDemo\\src\\main\\resources\\processes\\Developer_Hiring_with_jpa.bpmn";
+
+	@Rule
+	public ActivitiRule activitiRule = new ActivitiRule();
+
+	@Test
+	public void startProcess() throws Exception {
+		RepositoryService repositoryService = activitiRule.getRepositoryService();
+		repositoryService.createDeployment().addInputStream("hireProcessWithJpa.bpmn20.xml",
+				new FileInputStream(filename)).deploy();
+		RuntimeService runtimeService = activitiRule.getRuntimeService();
+		Map<String, Object> variableMap = new HashMap<String, Object>();
+		variableMap.put("name", "Activiti");
+		ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("hireProcessWithJpa", variableMap);
+		assertNotNull(processInstance.getId());
+		System.out.println("id " + processInstance.getId() + " "
+				+ processInstance.getProcessDefinitionId());
+	}
+}
